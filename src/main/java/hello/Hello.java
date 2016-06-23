e31df2e8f2696043dcd4c27f6d67cd1dcfb76025
abenmariem <abderrahmen.ben.mariem@gmail.com>
2016-06-23 16:42:48 +0100

    First commit

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,37 @@
+package hello;
+
+public class Hello {
+
+	private String firstName;
+	private String lastName;
+	
+	public Hello() {
+		super();
+		this.firstName = "ali";
+		this.lastName = "ben ali";
+	}
+		
+	public Hello(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void display(){
+		System.out.println("My FirstName "+ firstName + " My LastName "+ lastName);
+	}
+	
+}
---
+++ src/main/java/hello/MainHello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class MainHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hello h = new Hello("Saleh", "Ben Saleh");
+		h.display();
+		
+	}
+
+}
